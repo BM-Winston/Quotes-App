8d9c1b7f0f562f35cdcb75b0dc7f9f60b2f7f985
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-11 23:03:13 +0300

    Add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesZoneComponent } from './quotes-zone/quotes-zone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesZoneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes-zone/quotes-zone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes-zone',
+  templateUrl: './quotes-zone.component.html',
+  styleUrls: ['./quotes-zone.component.css']
+})
+export class QuotesZoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
