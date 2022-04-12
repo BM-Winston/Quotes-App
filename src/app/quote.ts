d847d5804c40746dba03a877e93d396f7d7ab88a
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-12 12:53:54 +0300

    Add initialiser

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,11 @@
 export class Quote {
-    constructor(id: number, quote:string, upvote:number, downvote:number){}
+    id : number;
+    quote : string;
+    upvote : number;
+    downvote : number;
+    constructor(id: number, quote:string, upvote:number, downvote:number){
+        this.id=id;
+        this.quote = quote;
+        this.upvote = upvote;
+        this.downvote = downvote;
 }
