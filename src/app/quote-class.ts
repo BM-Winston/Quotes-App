3de09ab28f6be30967763e77938976116617d997
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-21 11:22:21 +0300

    Auto-generated commit [ci skip]

--- src/app/quote-class.ts
+++
@@ -1,7 +0,0 @@
-export class QuoteClass {
-    showInfo:boolean=false;
-    constructor(public creator: string, public quote: string, public Author: string, public upvote:number, public downvote:number, public newDate:Date ){
-
-    }
-}
-
